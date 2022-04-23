57ced7548c94cc69d8e3745d336a66275f519fe1
Arza Aldi K <kampungvector@gmail.com>
2022-04-23 16:30:52 +0700

    test

--- jest.config.js
+++ jest.config.js
@@ -11,9 +11,9 @@ const customJestConfig = {
     moduleNameMapper: {
         // Handle module aliases (this will be automatically configured for you soon)
         '^@/components/(.*)$': '<rootDir>/components/$1',
-
         '^@/pages/(.*)$': '<rootDir>/pages/$1'
     },
+    testPathIgnorePatterns: ['/node_modules/'],
     testEnvironment: 'jest-environment-jsdom'
 }
 
